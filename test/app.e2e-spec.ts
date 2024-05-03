bfdba998705c13d9dd3c92e3e802f92b6ad212b3
Parsa Ghasemi <parsa.ghasemi2012@gmail.com>
2024-05-03 13:54:03 +0330

    e2e test template
    only implemented topics about what is gonna happen

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,131 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import {
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
+import { AppModule } from '../src/app.module';
+import { AuthDto } from '../src/auth/dto';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { SigninDto } from 'src/auth/dto/signin.dto';
 
-describe('AppController (e2e)', () => {
+describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+  beforeAll(async () => {
+    const moduleRef =
+      await Test.createTestingModule({
+        imports: [AppModule],
+      }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+    await app.listen(4000);
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl(
+      'http://localhost:4000',
+    );
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
   });
-});
+
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      username: 'parsa',
+      email: 'dummymail@gmail.com',
+      password: 'pass1234',
+      phoneNumber: '0123456789'
+    };
+    describe('Signup', () => {
+      it('should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            password: dto.password,
+            username: dto.username,
+            phoneNumber: dto.phoneNumber
+          })
+          .expectStatus(400);
+      });
+      describe('Signup', () => {
+        it('should throw if email is not email!!', () => {
+          return pactum
+            .spec()
+            .post('/auth/signup')
+            .withBody({
+              password: dto.password,
+              username: dto.username,
+              phoneNumber: dto.phoneNumber
+            })
+            .expectStatus(400);
+        });
+      it('should throw if password empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            username: dto.username,
+            email: dto.email,
+            phoneNumber: dto.phoneNumber
+          })
+          .expectStatus(400);
+      });
+      
+      it('should signup', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+
+  
+      describe('Signin', () => {
+        it('should throw if email empty', () => {
+          return pactum
+            .spec()
+            .post('/auth/signin')
+            .withBody({
+              password: dto.password,
+            })
+            .expectStatus(400);
+        });
+        it('should throw if password empty', () => {
+          return pactum
+            .spec()
+            .post('/auth/signin')
+            .withBody({
+              username: dto.username
+            })
+            .expectStatus(400);
+        });
+        it('should throw if no body provided', () => {
+          return pactum
+            .spec()
+            .post('/auth/signin')
+            .expectStatus(400);
+        });
+        it('should signin', () => {
+          return pactum
+            .spec()
+            .post('/auth/signin')
+            .withBody(dto)
+            .expectStatus(200)
+            .stores('userAt', 'access_token');
+        });
+      });
+    });
+  })
+})
