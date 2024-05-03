066acf407f3e7d16ba43cceca0dd405b3fd27891
Parsa Ghasemi <parsa.ghasemi2012@gmail.com>
2024-05-03 23:50:04 +0330

    fixed providers and small edits

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,10 @@
+import { UserService } from './user.service';
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { PaginationService } from 'src/service/paginator/pagination.service';
 
 @Module({
   controllers: [UserController],
-  providers: [PaginationService]
+  providers: [UserService, PaginationService]
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,3 +1,22 @@
 import { Injectable } from '@nestjs/common';
-@Injectable({})
-export class UserService {}
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
+import { UpdateUserDto } from 'src/auth/dto/update.user.dto';
+import { stringify } from 'querystring';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    return this.prisma.user.update({
+      where: { id },
+      data: updateUserDto
+    });
+  }
+  async remove(id: number): Promise<User> {
+    return this.prisma.user.delete({
+      where: { id },
+    });
+  }
+}
